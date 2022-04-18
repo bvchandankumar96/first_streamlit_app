5da5bebae21d10612582be7c526bf5fe81239763
Chandan Kumar <103919655+bvchandankumar96@users.noreply.github.com>
2022-04-18 11:44:35 +0530

    Update streamlit_app.py
    Additional Rows Details added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Mom\'s New Healty Dinner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & blueberry otameal")
+streamlit.text("kale,spinach and Rocket smoothi")
+streamlit.text("Hard Boiled, Free-Range egg")
